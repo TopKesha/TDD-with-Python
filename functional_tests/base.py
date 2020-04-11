00ebcc8e058bf208f3938180235698265d528717
Булгаков Роман <deregodub@gmail.com>
2020-04-11 17:43:21 +0700

    use form in all views, back to working state

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,7 +4,7 @@ import time
 from selenium.common.exceptions import WebDriverException
 import os
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
