662ee8edf9332780be79f283ffa70d904536429f
Булгаков Роман <deregodub@gmail.com>
2020-04-12 15:17:29 +0700

    Implement dublicate utem validation at model layer

--- lists/models.py
+++ lists/models.py
@@ -9,5 +9,11 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.DO_NOTHING)
 
+    class Meta:
+        ordering = ("id",)
+        unique_together = ('list','text')
+
+    def __str__(self):
+        return self.text
 
 # Create your models here.
