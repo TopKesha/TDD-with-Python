3380afd1c9281582536713c312b3024304e4be4c
Булгаков Роман <deregodub@gmail.com>
2020-04-20 17:36:44 +0700

    API + JS errors

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import re_path, include
 from lists import views as list_views
 from lists import urls as list_urls
+from lists import api_urls
 from accounts import urls as accounts_urls
 
 urlpatterns = [
     re_path(r'^$', list_views.HomePageView.as_view(), name='home'),
     re_path(r'^lists/', include(list_urls)),
-    re_path(r'^accounts/', include(accounts_urls))
+    re_path(r'^accounts/', include(accounts_urls)),
+    re_path(r'^api/', include(api_urls)),
 ]
